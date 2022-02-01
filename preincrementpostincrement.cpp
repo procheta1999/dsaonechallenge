01e97f32ba4ccbbb20df75916b83259d8b6483fd
Procheta Bhattacharyya <53190270+procheta1999@users.noreply.github.com>
2022-02-01 23:13:10 +0530

    Create preincrementpostincrement.cpp

---
+++ preincrementpostincrement.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int a=9;
+    int b=7;
+    cout<<++a<<endl; //10
+    cout<<b++<<endl; //7
+    cout<<--b<<endl; //7
+    cout<<a<<endl; //10
+    cout<<b<<endl; //7
+    return 0;
+}
